099955cc426de38c0d2af3abdb46c24ed5c043c6
unknown <nelson_wc@hotmail.com>
2022-11-22 21:31:34 -0500

    crear el repository y las clases de servicio y el metodo show de ProductoController

---
+++ src/main/java/com/DAA2/service/ProductoService.java
@@ -0,0 +1,12 @@
+package com.DAA2.service;
+
+import java.util.Optional;
+
+import com.DAA2.model.Producto;
+
+public interface ProductoService {
+	public Producto save(Producto producto);
+	public Optional<Producto>get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+}
